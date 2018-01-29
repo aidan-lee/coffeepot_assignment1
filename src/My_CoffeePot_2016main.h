bc60a2be2fec314de87c5fe845004239d7f1868f
aidan-lee <35906479+aidan-lee@users.noreply.github.com>
2018-01-28 20:33:26 -0700

    Assignment code uploaded

---
+++ src/My_CoffeePot_2016main.h
@@ -0,0 +1,60 @@
+/*****************************************************************************
+ * My_CoffeePot_2017Assignment.h
+ *****************************************************************************/
+
+#ifndef __MY_COFFEEPOT_2017ASSIGNMENT_H__
+#define __MY_COFFEEPOT_2017ASSIGNMENT_H__
+
+/*
+ * Compilation Instructions
+ *
+ * To test Parts 1, 2/3, and 4/ 5
+ * -> Set Parts12345 to 1
+ *
+ * 		To test Part 2
+ * 		-> Set Part2 to 1
+ * 		-> Set all others to 0
+ *
+ * 		To test Part 3
+ * 		-> Set Part3 to 1
+ * 		-> Set all others to 0
+ *
+ * 		To test Part 4
+ * 		-> Set Part4 to 1
+ * 		-> Set all others to 0
+ *
+ * 		To Test Part 5
+ * 		-> Set Part5 to 1
+ * 		-> Set all others to 0
+ *
+ *
+ * To test Part 6
+ * -> Set MultiplePots to 1
+ * -> Set Part6 to 1
+ * -> Set all others to 0
+ *
+ * To test Part 7 with 3 pots
+ * -> Set MultiplePots to 1
+ * -> Set Part7 to 1
+ * -> Set threePotsAtOnce to 1
+ * -> Set all others to 0
+ *
+ * To test Part 7 with 4 pots
+ * -> Set MultiplePots to 1
+ * -> Set Part7 to 1
+ * -> Set fourPotsAtOnce to 1
+ * -> Set all others to 0
+ *
+ */
+#define Parts12345 1
+#define Part2 0
+#define Part3 1
+#define Part4 0
+#define Part5 1
+#define MultiplePots 0
+#define Part6 0
+#define Part7 0
+#define threePotsAtOnce 0
+#define fourPotsAtOnce 1
+
+#endif /* __MY_COFFEEPOT_2017ASSIGNMENT_H__ */
